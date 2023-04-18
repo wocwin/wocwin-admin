f9dfcf972ad2c99bf0668d51dda8348efe48b150
wocwin <wocwin@163.com>
2023-04-18 21:28:07 +0800

    feat:新增gif图片

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,13 @@
 // ? 全局默认配置项
 
 // 首页地址（默认）
-export const HOME_URL: string = "/home/index";
+export const HOME_URL: string = "/index";
 
 // 登录页地址（默认）
 export const LOGIN_URL: string = "/login";
 
 // 默认主题颜色
-export const DEFAULT_PRIMARY: string = "#009688";
+export const DEFAULT_PRIMARY: string = "#142969";
 
 // 路由白名单地址（必须是本地存在的路由 staticRouter.ts 中）
 export const ROUTER_WHITE_LIST: string[] = ["/500"];
