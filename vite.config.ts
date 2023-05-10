3e717522ecd83c04394783f0e93b039815d58763
wocwin <wocwin@163.com>
2023-05-10 12:47:23 +0800

    fix:build打包更新

--- vite.config.ts
+++ vite.config.ts
@@ -64,26 +64,36 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
     },
     plugins: createVitePlugins(),
     build: {
-      target: "modules",
       outDir: "dist",
       minify: "esbuild", // esbuild打包速度最快，terser打包体积最小。
       // assetsInlineLimit: 4000, // 小于该值 图片将打包成Base64
-      // 禁用 gzip 压缩大小报告，可略微减少打包时间
-      // reportCompressedSize: false,
+      terserOptions: {
+        compress: {
+          // warnings: false,
+          drop_console: true, //打包时删除console
+          drop_debugger: true, //打包时删除 debugger
+          pure_funcs: ["console.log"]
+        },
+        output: {
+          // 去掉注释内容
+          comments: true
+        }
+      },
+      reportCompressedSize: false, // 禁用 gzip 压缩大小报告，可略微减少打包时间
       // 规定触发警告的 chunk 大小
-      chunkSizeWarningLimit: 3000,
+      chunkSizeWarningLimit: 2000,
       rollupOptions: {
         output: {
           // js最小拆包
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              return id.toString().split("node_modules/")[1].split("/")[0].toString();
-            }
-          }
+          // manualChunks(id) {
+          //   if (id.includes("node_modules")) {
+          //     return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          //   }
+          // },
           // 静态资源分类和包装
-          // chunkFileNames: "assets/js/[name]-[hash].js",
-          // entryFileNames: "assets/js/[name]-[hash].js",
-          // assetFileNames: "assets/[ext]/[name]-[hash].[ext]"
+          chunkFileNames: "assets/js/[name]-[hash].js",
+          entryFileNames: "assets/js/[name]-[hash].js",
+          assetFileNames: "assets/[ext]/[name]-[hash].[ext]"
         }
       }
     }
