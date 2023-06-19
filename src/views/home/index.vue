803e0190bde0a08518a59206ec1d744886846d26
wocwin <wocwin@163.com>
2023-06-19 18:16:49 +0800

    refactor:首页显示Vue/Vite使用版本

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -13,6 +13,17 @@
                 {{ username || "wocwin" }}
               </span>
             </div>
+            <div class="flex-box flex-ver-v" style="margin-top: 5px">
+              <a style="padding: 0 5px" href="https://github.com/vuejs/vue" target="_blank">
+                <img src="https://img.shields.io/badge/vue-3.2.47-brightgreen.svg" alt="vue3" />
+              </a>
+              <a style="padding: 0 5px" href="https://github.com/vitejs/vite" target="_blank">
+                <img src="https://img.shields.io/badge/vite-4.3.9-brightgreen.svg" alt="vite4" />
+              </a>
+              <a style="padding: 0 5px" href="https://github.com/wocwin/wocwin-admin/stargazers" target="_blank">
+                <img src="https://img.shields.io/github/stars/wocwin/wocwin-admin.svg" alt="wocwin-admin" />
+              </a>
+            </div>
             <div class="space-x-2 flex-box flex-ver-v">
               <el-divider direction="vertical" />
               <el-link target="_blank" type="success" href="https://gitee.com/wocwin/wocwin-admin">Gitee源码</el-link>
